ce4f2d1f4dcae56c224437af0561fa87e176bd2f
Tomoatsu Shimada <shimarin@stbbs.net>
2012-11-16 14:04:06 +0530

    debootstrap script for debian improved

--- distfiles/debian/debootstrap.sh
+++ distfiles/debian/debootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-mkdir tmp
-debootstrap --arch=i386 --include=openssh-server,avahi-daemon,samba,ntp,openvpn,ca-certificates squeeze ./tmp
+ARCH=i386
+PACKAGES=openssh-server,avahi-daemon,samba,ntp,openvpn,ca-certificates,locales-all 
+DIST=squeeze
+mkdir tmp || exit
+debootstrap --arch=$ARCH --include=$PACKAGES $DIST ./tmp || exit
 chroot ./tmp apt-get clean
 tar Jcvpf debian-i686.tar.xz -C ./tmp .
 rm -rf tmp

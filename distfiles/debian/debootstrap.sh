88d80a8df5ae3baa29b333a6242d6fa72dc025aa
Tomoatsu Shimada <shimada@walbrix.com>
2013-09-11 13:26:22 +0900

    old debootstrap scripe no longer needed

--- distfiles/debian/debootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ARCH=i386
-PACKAGES=openssh-server,avahi-daemon,samba,ntp,openvpn,ca-certificates,locales-all 
-DIST=wheezy
-mkdir tmp || exit
-debootstrap --arch=$ARCH --include=$PACKAGES $DIST ./tmp || exit
-chroot ./tmp apt-get clean
-tar Jcvpf debian-i686.tar.xz -C ./tmp .
-rm -rf tmp

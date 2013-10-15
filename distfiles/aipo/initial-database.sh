7efd919ca169f071a69985c89ffc15bc26a3e3e6
Tomoatsu Shimada <shimada@walbrix.com>
2013-10-16 02:48:44 +0900

    aipo 7.0.2.0

--- distfiles/aipo/initial-database.sh
+++ distfiles/aipo/initial-database.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
+PGSQL_VERSION=9.2
+source /etc/conf.d/postgresql-$PGSQL_VERSION
+PGDATA=/etc/postgresql-$PGSQL_VERSION/
+DATA_DIR=/var/lib/postgresql/$PGSQL_VERSION/data
+
 USER=aipo_postgres
 DB=org001
-PGDIR=/usr/lib/postgresql-9.1
-$PGDIR/bin/createuser -SDR $USER -U postgres
-$PGDIR/bin/createdb $DB -E utf-8 -T template0 -O $USER -U postgres
-$PGDIR/bin/psql -U $USER -d $DB -f aipo/src/initdata/sql/org001.sql
-$PGDIR/bin/psql -U $USER -d $DB -f aipo/src/initdata/sql/sample.sql
-$PGDIR/bin/psql -U $USER -d $DB -c "UPDATE EIP_M_COMPANY SET PORT_INTERNAL=80"
-$PGDIR/bin/psql -U $USER -d $DB -c "UPDATE AIPO_LICENSE SET LIMIT_USERS=0"
-$PGDIR/bin/psql -U $USER -d $DB -c "INSERT INTO eip_m_mail_account VALUES (1, 1, 'aipo', '1', 'localhost', '', '', '\x7a540f5868b3e117', 'aipo', 'aipo@example.com', '25', 0, '110', 0, 0, '', '\x7a540f5868b3e117', 0, '0', '0', 0, '1', 1, NULL, NULL, now(), now())"
-/etc/init.d/postgresql-9.1 stop
-tar zcvpf initial-database.tar.gz --exclude='data/postmaster.*' -C /var/lib/postgresql/9.1 data
+
+su postgres -c "postgres -D ${PGDATA} --data-directory=${DATA_DIR}" &
+PID=$!
+
+sleep 5
+
+createuser -SDR $USER -U postgres
+createdb $DB -E utf-8 -T template0 -O $USER -U postgres
+psql -U $USER -d $DB -f /root/aipo/src/initdata/sql/org001.sql
+psql -U $USER -d $DB -f /root/aipo/src/initdata/sql/sample.sql
+psql -U $USER -d $DB -c "UPDATE EIP_M_COMPANY SET PORT_INTERNAL=80"
+psql -U $USER -d $DB -c "UPDATE AIPO_LICENSE SET LIMIT_USERS=0"
+psql -U $USER -d $DB -c "INSERT INTO eip_m_mail_account VALUES (1, 1, 'aipo', '1', 'localhost', '', '', '\x7a540f5868b3e117', 'aipo', 'aipo@example.com', '25', 0, '110', 0, 0, '', '\x7a540f5868b3e117', 0, '0', '0', 0, '1', 1, NULL, NULL, now(), now())"
+
+kill $PID
+wait
 

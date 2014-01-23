8763777bc608aca4d1e2d41108160419ad3c06d0
Tomoatsu Shimada <shimada@walbrix.com>
2014-01-23 15:33:45 +0900

    glassfish 4.0

--- distfiles/glassfish/glassfish-src.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-VERSION=3.1.2.2
-svn export https://svn.java.net/svn/glassfish~svn/tags/$VERSION-no-delete glassfish-$VERSION
-tar Jcvpf glassfish-$VERSION-src.tar.xz --numeric-owner glassfish-$VERSION
-

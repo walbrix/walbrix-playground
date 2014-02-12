59ba64d7ae4f2877642e2d600fe7cac3e8dd37af
Tomoatsu Shimada <shimada@walbrix.com>
2014-02-12 12:45:18 +0900

    zabbix 2.2.0

--- distfiles/zabbix/initial-database-ja.sh
+++
@@ -1,8 +0,0 @@
-mysql -e "create database zabbix"
-mysql -e 'grant all privileges on zabbix.* to zabbix@localhost'
-mysql zabbix < /usr/share/zabbix/database/mysql/schema.sql
-mysql zabbix < /usr/share/zabbix/database/mysql/images.sql 
-mysql zabbix < /usr/share/zabbix/database/mysql/data.sql   
-mysql zabbix -e "update users set lang='ja_JP' where userid=1"
-tar zcvpf initial-database-ja.tar.gz -C /var/lib/mysql ib_logfile0 ib_logfile1 ibdata1 mysql/db.MYD mysql/db.MYI mysql/user.MYI mysql/user.MYD zabbix
-mv initial-database-ja.tar.gz /tmp/
---
+++ distfiles/zabbix/initial-database-ja.sql
@@ -0,0 +1,3 @@
+update users set lang='ja_JP';
+update media_type set smtp_server='localhost',smtp_helo='example.com',smtp_email='zabbix@example.com' where type=0;
+

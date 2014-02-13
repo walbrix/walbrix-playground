99bf7931a57a7d01a52183a502d4d920f7a8aa5c
Tomoatsu Shimada <shimada@walbrix.com>
2014-02-13 12:08:51 +0900

    zabbix server watches itself as well

--- distfiles/zabbix/initial-database-ja.sql
+++ distfiles/zabbix/initial-database-ja.sql
@@ -1,3 +1,4 @@
 update users set lang='ja_JP';
 update media_type set smtp_server='localhost',smtp_helo='example.com',smtp_email='zabbix@example.com' where type=0;
+update hosts set status=0 where host='Zabbix server';
 

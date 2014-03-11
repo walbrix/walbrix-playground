6df0435c122a761d0614498e4116fb0a7dc84b00
Tomoatsu Shimada <shimada@walbrix.com>
2014-03-11 16:32:08 +0900

    centos x86_64

--- distfiles/centos/build-minimal.sh
+++ distfiles/centos/build-minimal.sh
@@ -10,21 +10,21 @@ ARCH=`echo $RELEASE_RPM|sed 's/.*\.\(.*\)\.rpm$/\1/'`
 RELEASE=`echo $RELEASE_RPM|sed 's/.*\/\([0-9\.]*\)\/os\/.*\/Packages\/.*\.rpm$/\1/'`
 echo $ARCH $RELEASE
 
-PREFIX=i386
+[ $ARCH = "i686" ] && PREFIX=i386 || PREFIX=$ARCH
 
 rm -rf /tmp/centos-$RELEASE-$ARCH
 mkdir -p /tmp/centos-$RELEASE-$ARCH/var/lib/rpm
-$PREFIX rpm --root /tmp/centos-$RELEASE-$ARCH --initdb
+$PREFIX rpm --root /tmp/centos-$RELEASE-$ARCH --initdb || exit 1
 $PREFIX rpm -ivh --nodeps --root /tmp/centos-$RELEASE-$ARCH $RELEASE_RPM
-$PREFIX yum --nogpgcheck --installroot /tmp/centos-$RELEASE-$ARCH install -y yum || exit
-rm /tmp/centos-$RELEASE-$ARCH/var/lib/rpm/*
-rm -rf /tmp/centos-$RELEASE-$ARCH/var/cache/yum
+$PREFIX yum --nogpgcheck --installroot /tmp/centos-$RELEASE-$ARCH install -y yum || exit 1
+rm -f /tmp/centos-$RELEASE-$ARCH/var/lib/rpm/*
+rm -rf /tmp/centos-$RELEASE-$ARCH/var/cache/yum || exit 1
 echo "nameserver 8.8.8.8" > /tmp/centos-$RELEASE-$ARCH/etc/resolv.conf
 $PREFIX chroot /tmp/centos-$RELEASE-$ARCH rpm --initdb
 $PREFIX chroot /tmp/centos-$RELEASE-$ARCH rpm -ivh $RELEASE_RPM
 $PREFIX chroot /tmp/centos-$RELEASE-$ARCH yum install -y yum
 rm /tmp/centos-$RELEASE-$ARCH/var/cache/yum/base/packages/*
-$PREFIX chroot /tmp/centos-$RELEASE-$ARCH yum install -y vim-minimal less initscripts passwd tar
-tar Jcvpf centos-$RELEASE-$ARCH.tar.xz -C /tmp/centos-$RELEASE-$ARCH .
+$PREFIX chroot /tmp/centos-$RELEASE-$ARCH yum install -y vim-minimal less initscripts passwd tar || exit 1
+tar Jcvpf centos-$RELEASE-$ARCH.tar.xz -C /tmp/centos-$RELEASE-$ARCH . || exit 1
 rm -rf /tmp/centos-$RELEASE-$ARCH
 

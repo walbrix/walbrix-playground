62993fce25397f55bfbe57f2511059687737c58d
Tomoatsu Shimada <shimada@walbrix.com>
2014-04-06 15:09:46 +0900

    redmine 2.5.1

--- distfiles/redmine/passwd.rb
+++ distfiles/redmine/passwd.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 require 'yaml'
-require 'mysql'
+require 'mysql2'
 require 'digest/sha1'
 
 dbconfig = YAML::load(File.open("/var/www/localhost/htdocs/config/database.yml"))["production"]
@@ -14,16 +14,16 @@ if host == nil then
   host = 'localhost'
 end
 
-client = Mysql.connect(host, username, dbpassword, dbname)
+client = Mysql2::Client::new(:host=>host, :username=>username, :password=>dbpassword, :database=>dbname)
 
 login = gets.strip
 password = gets.strip
 
-stmt = client.prepare("select hashed_password,salt from users where type='User' and login=?")
-stmt.execute(login).each { |hashed_password, salt|
+stmt = client.query("select hashed_password,salt from users where type='User' and login='#{client.escape(login)}'").each { |row|
+  hashed_password = row["hashed_password"]
+  salt = row["salt"]
   exit(2) if hashed_password == nil || salt == nil
   exit(1) if Digest::SHA1.hexdigest(salt+Digest::SHA1.hexdigest(password)) != hashed_password
   exit(0)
 }
 exit(2)
-

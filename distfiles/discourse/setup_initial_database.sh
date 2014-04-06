5efcd812d78c28788f3840c5131168cb08c78056
Tomoatsu Shimada <shimada@walbrix.com>
2014-04-06 20:18:30 +0900

    discourse 0.9.9

--- distfiles/discourse/setup_initial_database.sh
+++ distfiles/discourse/setup_initial_database.sh
@@ -10,14 +10,20 @@ DB=$USER
 su - postgres -c "postgres -D ${PGDATA} --data-directory=${DATA_DIR}" &
 PID=$!
 
+redis-server &
+REDIS_PID=$!
+
 sleep 5
 
-createuser -SDR $USER -U postgres
+createuser -sDR $USER -U postgres
 createdb $DB -E utf-8 -T template0 -O $USER -U postgres
 
 cd /var/www/localhost/htdocs
-#RUBY_GC_MALLOC_LIMIT=90000000 RAILS_ENV=production bundle exec rake db:migrate
+RUBY_GC_MALLOC_LIMIT=90000000 RAILS_ENV=production bundle exec rake db:migrate
+RUBY_GC_MALLOC_LIMIT=90000000 RAILS_ENV=production bundle exec rake assets:precompile
+RUBY_GC_MALLOC_LIMIT=90000000 RAILS_ENV=production bundle exec rails r 'user=User.find(-1);user.password="admin";user.save;user.email_tokens.each{|token|token.confirmed=true;token.save}'
 
+kill $REDIS_PID
 kill $PID
 wait
-
+rm -f /dump.rdb

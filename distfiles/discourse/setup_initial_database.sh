23e02e6e0ae9d65184a9356ba97bb48b74f094ce
Tomoatsu Shimada <shimada@walbrix.com>
2014-04-07 09:33:02 +0900

    discourse can never be useable under 384MB RAM

--- distfiles/discourse/setup_initial_database.sh
+++ distfiles/discourse/setup_initial_database.sh
@@ -18,6 +18,17 @@ sleep 5
 createuser -sDR $USER -U postgres
 createdb $DB -E utf-8 -T template0 -O $USER -U postgres
 
+wget -O - http://pgfoundry.org/frs/download.php/2943/textsearch_ja-9.0.0.tar.gz | tar zxvf - -C /
+cd /textsearch_ja-9.0.0
+make USE_PGXS=1
+make USE_PGXS=1 install
+cp textsearch_ja.sql /tmp/
+su - postgres -c "psql -f /tmp/textsearch_ja.sql discourse"
+rm /tmp/textsearch_ja.sql
+cd /
+rm -rf textsearch_ja-9.0.0
+read -n1 -r -p "Press any key to continue..." key
+
 cd /var/www/localhost/htdocs
 RUBY_GC_MALLOC_LIMIT=90000000 RAILS_ENV=production bundle exec rake db:migrate
 RUBY_GC_MALLOC_LIMIT=90000000 RAILS_ENV=production bundle exec rake assets:precompile

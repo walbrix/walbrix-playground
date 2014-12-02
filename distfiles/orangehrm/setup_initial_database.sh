9b1679124462d40ee7dbac0fbf28f2927e845d2e
Tomoatsu Shimada <shimada@walbrix.com>
2014-12-02 21:35:05 +0900

    OrangeHRM 3.2.1

---
+++ distfiles/orangehrm/setup_initial_database.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+MY_DATADIR=/var/lib/mysql
+SOCKET=/var/run/mysqld/mysqld.sock
+PIDFILE=/tmp/mysql.pid
+/usr/sbin/mysqld --skip-networking --user=mysql --log-warnings=0 --basedir=/usr\
+ --datadir=$MY_DATADIR --max_allowed_packet=8M --net_buffer_length=16K\
+ --default-storage-engine=MyISAM --socket=$SOCKET --pid-file=$PIDFILE &
+maxtry=15
+while ! [[ -S "$SOCKET" || "${maxtry}" -lt 1 ]] ; do
+	maxtry=$((${maxtry}-1))
+	echo -n "."
+	sleep 1
+done
+if ! [[ -S "$SOCKET" ]]; then
+	echo "Failed to start mysqld"
+	exit 1
+fi
+/usr/bin/mysql -u root -e "create database orangehrm" || exit 1
+/usr/bin/mysql -u root -e 'grant all privileges on orangehrm.* to orangehrm@localhost' || exit 1
+/usr/bin/mysql -u root orangehrm < /tmp/initial_database.sql
+
+kill `cat $PIDFILE`
+rm -f $PIDFILE
+while [[ -S "$SOCKET" ]] ; do
+	echo -n "."
+	sleep 1
+done

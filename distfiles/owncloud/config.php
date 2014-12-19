217fd790f07e888b23b9cee38fd2043fef0785c7
Tomoatsu Shimada <shimada@walbrix.com>
2014-12-19 19:33:03 +0900

    trusted_domains removed

--- distfiles/owncloud/config.php
+++ distfiles/owncloud/config.php
@@ -4,9 +4,6 @@ $CONFIG = array (
   'passwordsalt' => 'ea217424fc6d97709c4cb3234b909f',
   'secret' => 'e4008d1926fb55846f5a4fc1a66b44ff5a82c52f83eafecce532052c9b333a8ba660591b0e086a9330603a3a6dd4c854',
   'trusted_domains' => 
-  array (
-    0 => 'owncloud.local',
-  ),
   'datadirectory' => '/var/www/localhost/htdocs/data',
   'dbtype' => 'mysql',
   'version' => '7.0.4.2',

897d5364e46b637eae4bdab71053461fc14c5b7d
Tomoatsu Shimada <shimada@walbrix.com>
2015-02-07 00:26:24 +0900

    remove an insert statement which doesn't work

--- distfiles/tt-rss/setup_initial_database.sh
+++ distfiles/tt-rss/setup_initial_database.sh
@@ -19,7 +19,7 @@ fi
 /usr/bin/mysql -u root -e 'grant all privileges on `tt-rss`.* to '\''tt-rss'\''@localhost' || exit 1
 /usr/bin/mysql -u tt-rss tt-rss < /var/www/localhost/htdocs/schema/ttrss_schema_mysql.sql
 /usr/bin/mysql -u tt-rss tt-rss -e "update ttrss_prefs set def_value='Asia/Tokyo' where pref_name='USER_TIMEZONE'"
-/usr/bin/mysql -u tt-rss tt-rss -e "insert into ttrss_feeds (owner_uid, title, feed_url) values (1, 'ワルブリックス株式会社 ブログ', 'http://www.walbrix.com/jp/blog/index.xml')"
+#/usr/bin/mysql -u tt-rss tt-rss -e "insert into ttrss_feeds (owner_uid, title, feed_url) values (1, 'ワルブリックス株式会社 ブログ', 'http://www.walbrix.com/jp/blog/index.xml')"
 
 kill `cat $PIDFILE`
 rm -f $PIDFILE

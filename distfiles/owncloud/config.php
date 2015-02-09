cb6144e783331bbdc53959d909f859887d945f10
Tomoatsu Shimada <shimada@walbrix.com>
2015-02-09 11:05:56 +0900

    fix config.php error

--- distfiles/owncloud/config.php
+++ distfiles/owncloud/config.php
@@ -3,7 +3,6 @@ $CONFIG = array (
   'instanceid' => 'oc10f1587b8f',
   'passwordsalt' => 'ea217424fc6d97709c4cb3234b909f',
   'secret' => 'e4008d1926fb55846f5a4fc1a66b44ff5a82c52f83eafecce532052c9b333a8ba660591b0e086a9330603a3a6dd4c854',
-  'trusted_domains' => 
   'datadirectory' => '/var/www/localhost/htdocs/data',
   'dbtype' => 'mysql',
   'version' => '7.0.4.2',

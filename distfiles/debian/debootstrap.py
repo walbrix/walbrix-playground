28a77d95f22c1a2f342e26e132b50447e12298f2
Tomoatsu Shimada <shimada@walbrix.com>
2015-03-09 10:24:49 +0900

    changing prefix (e.g. between 'debian' and 'ubuntu')

--- distfiles/debian/debootstrap.py
+++ distfiles/debian/debootstrap.py
@@ -12,12 +12,12 @@ def shell_exec(cmd):
     process = subprocess.Popen(cmd, shell=True)
     if process.wait() != 0: raise Exception("Subprocess '%s' returned error")
 
-def debootstrap(arch, dist, packages):
+def debootstrap(arch, dist, packages, prefix="debian"):
     tmpdir = tempfile.mkdtemp()
     try:
-        shell_exec("debootstrap --arch=%s --include=%s %s %s" % (arch_strings[arch], packages, dist, tmpdir))
+        shell_exec("debootstrap --no-check-gpg --arch=%s --include=%s %s %s" % (arch_strings[arch], packages, dist, tmpdir))
         shell_exec("chroot %s apt-get clean" % tmpdir)
-        shell_exec("tar Jcvpf debian-%s.tar.xz -C %s ." % (arch, tmpdir))
+        shell_exec("tar Jcvpf %s-%s.tar.xz -C %s ." % (prefix, arch, tmpdir))
     finally:
         shutil.rmtree(tmpdir)
 
@@ -28,6 +28,10 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if args.arch not in arch_strings: raise Exception("Invalid architecture name")
-    debootstrap(args.arch, args.dist, "openssh-server,avahi-daemon,samba,ntp,openvpn,ca-certificates,locales-all")
+    if args.arch not in arch_strings:
+        raise Exception("Invalid architecture name")
+    pkgs = "openssh-server,avahi-daemon,samba,ntp,openvpn,ca-certificates"
+    is_ubuntu = os.system("grep -q ubuntu /usr/share/debootstrap/scripts/%s" % args.dist) == 0
+    if not is_ubuntu: pkgs += ",locales-all"
+    debootstrap(args.arch, args.dist, pkgs, "ubuntu" if is_ubuntu else "debian")
 
